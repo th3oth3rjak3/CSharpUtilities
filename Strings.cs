811c063ef0834821525b4ec5c68ed88d97dca3f9
Jake Hathaway <77807678+th3oth3rjak3@users.noreply.github.com>
2023-02-25 18:25:38 -0800

    create new utilities library.

---
+++ Strings.cs
@@ -0,0 +1,10 @@
+namespace Utilities;
+
+public class StringFunctions
+{
+    /// <summary>
+    /// Prints a string to the console.
+    /// </summary>
+    /// <param name="s">The string to print.</param>
+    public static void Print(String s) => Console.WriteLine(s);
+}
